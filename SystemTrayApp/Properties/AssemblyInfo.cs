305dd6b0342170bd0284579a5061dde156f716ce
DJG <dave.gandy@greenhippo.co.uk>
2018-12-20 18:09:58 +0000

    More Tidying!

--- SystemTrayApp/Properties/AssemblyInfo.cs
+++ SystemTrayApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SystemTrayApp")]
+[assembly: AssemblyTitle("Wiser System Tray App")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SystemTrayApp")]
+[assembly: AssemblyProduct("Wiser System Tray App")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
